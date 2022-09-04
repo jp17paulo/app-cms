d1dc5d0adbb4b72e1cf2f7c1f9e5a2ec398eb54e
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-09-04 01:58:47 -0300

    Inclusão de menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -11,7 +11,7 @@ export class MenuComponent implements OnInit {
   constructor(private menu: MenuController) { }
 
   ngOnInit() {
-    this.openFirst();
+    // this.openFirst();
   }
 
   openFirst() {
